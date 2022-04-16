6c0c42ffe20495ebcf4250772c4dacbf869465e7
Riley Dixon <rileydixon@Rileys-MacBook-Pro.local>
2022-04-16 13:38:51 -0400

    heroku commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,42 @@
+// export const REST = {
+//   getBoard: function () {
+//     return fetch(`http://localhost:4000/puzzle`);
+//   },
+//   solveBoard: function (grid) {
+//     const data = {
+//       board: grid,
+//     };
+//     return fetch(`http://localhost:4000/solve`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     });
+//   },
+//   validateBoard: function (grid) {
+//     const data = {
+//       board: grid,
+//     };
+//     return fetch(`http://localhost:4000/validate`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     });
+//   },
+// };
+
 export const REST = {
   getBoard: function () {
-    return fetch(`http://localhost:4000/puzzle`);
+    return fetch(`https://rjd-capstone-backend.herokuapp.com/puzzle`);
   },
   solveBoard: function (grid) {
     const data = {
       board: grid,
     };
-    return fetch(`http://localhost:4000/solve`, {
+    return fetch(`https://rjd-capstone-backend.herokuapp.com/solve`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -18,7 +48,7 @@ export const REST = {
     const data = {
       board: grid,
     };
-    return fetch(`http://localhost:4000/validate`, {
+    return fetch(`https://rjd-capstone-backend.herokuapp.com/validate`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
